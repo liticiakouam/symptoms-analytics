2f042b2267e7474e97699aedfa7505963d6bcc49
liticiakouam <anzeliticia@gmail.com>
2022-07-13 18:41:44 +0100

    Recupération de la liste des symptomes dépuis un fichier

--- src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -1,7 +1,11 @@
 package com.hemebiotech.analytics;
 
+import java.util.List;
+
 public class AnalyticsCounter {
 	public static void main(String args[]) {
-
+		//lecture des symptomes
+		ISymptomReader symptomReader = new ReadSymptomDataFromFile("symptoms.txt");
+		List<String> symptoms = symptomReader.getSymptoms();
 	}
 }

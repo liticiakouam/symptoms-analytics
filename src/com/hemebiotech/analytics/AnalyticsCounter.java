4339b0ca920534d5d2a2a4de4ea5a401216faf1c
liticiakouam <anzeliticia@gmail.com>
2022-07-13 18:35:33 +0100

    Nettoyage du code

--- src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -1,11 +1,7 @@
 package com.hemebiotech.analytics;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.FileWriter;
-
 public class AnalyticsCounter {
-	public static void main(String args[]) throws Exception {
+	public static void main(String args[]) {
 
 	}
 }

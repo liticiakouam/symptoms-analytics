6c7a1c0ebd207b79dbdd997ce280a4da1463481d
liticiakouam <anzeliticia@gmail.com>
2022-07-13 19:36:31 +0100

    Ecriture des symptomes dans le fichier result.out

--- src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -1,10 +1,11 @@
 package com.hemebiotech.analytics;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.Map;
 
 public class AnalyticsCounter {
-	public static void main(String args[]) {
+	public static void main(String[] args) throws IOException {
 		// Lecture des symptomes
 		ISymptomReader symptomReader = new ReadSymptomDataFromFile("symptoms.txt");
 		List<String> symptoms = symptomReader.getSymptoms();
@@ -12,5 +13,9 @@ public class AnalyticsCounter {
 		//comptage du nombre d'occurrences de chaque symptome
 		ISymptomsCount counter = new SymptomsCount();
 		Map<String, Integer> symptomsWithOccurrences = counter.countSymptoms(symptoms);
+
+		//ecriture dans le fichier de sortie
+		ISymptomWrite symptomWrite = new WriteSymptomDataToFile("result.out");
+		symptomWrite.writeToFile(symptomsWithOccurrences);
 	}
 }
